1179d18c3913ba25bd3317a24250a7bbf344cd04
BextiyarNovruzov <baxtiyarbn-bp215@code.edu.az>
2024-10-17 05:05:20 +0400

    Class Tasks part1

---
+++ ClassTask1.cs
@@ -0,0 +1,65 @@
+﻿using System.Reflection;
+using System.Xml.Schema;
+
+namespace class_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Task1
+            Car MyCar = new Car("Jeep", "Wrangler", 1920, 2);
+            MyCar.ShowFullInfo();
+            MotorCycle myBike = new MotorCycle("Yamaha", "R15", 8000, 2);
+            myBike.ShowFullInfo();
+
+        }
+    }
+
+
+    class Vehicle
+    {
+        public string Brand;
+        public string Model;
+        public double Mile;
+
+        public Vehicle(string Brand, string Model, double Mile)
+        {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.Mile = Mile;
+        }
+
+    }
+
+    class Car : Vehicle
+    {
+        public int DoorsNum;
+
+        public Car(string Brand, string Model, double Mile, int DoorsNum) : base(Brand, Model, Mile)
+        {
+            this.DoorsNum = DoorsNum;
+        }
+        public void ShowFullInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} Model:{Model} Mile:{Mile} DoorsNume:{DoorsNum}");
+        }
+    }
+
+    class MotorCycle : Vehicle
+    {
+        public int WheelNum;
+
+        public MotorCycle(string Brand, string Model, double Mile, int WheelNum) : base(Brand, Model, Mile)
+        {
+            this.WheelNum = WheelNum;
+        }
+        public void ShowFullInfo()
+        {
+            Console.WriteLine($"Brand: {Brand} Model:{Model} WheelNum:{WheelNum}");
+        }
+    }
+
+}
+
+
---
+++ ClassTask2.cs
@@ -0,0 +1,83 @@
+﻿using System.Reflection;
+using System.Xml.Schema;
+
+namespace class_task 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   ////Task2
+            Notebook notebook = new Notebook("Apple","MacBook Pro 16",1,true,8500, 1 ,16);
+            notebook.Sale();
+            notebook.ShowFullData();
+
+        }
+    }
+
+    class Product
+    {
+        public string Name;
+        public string Description;
+        public int Count;
+        public bool IsStock;
+        public double Price;
+
+        public Product(string Name, string Description, int Count, bool IsStock, double price)
+        {
+            this.Name = Name;
+            this.Description = Description;
+            this.Count = Count;
+            this.IsStock = IsStock;
+            this.Price = price;
+        }
+
+
+    }
+
+    class Notebook : Product
+    {
+        public byte RAM;
+        public int Storage;
+
+        public Notebook(string Name, string Description, int Count, bool IsStock, double price, byte RAM, int Storege)
+            : base(Name, Description, Count, IsStock, price)
+        {
+            this.RAM = RAM;
+            this.Storage = Storege;
+        }
+
+        public void Sale()
+        {
+            if (IsStock == false)
+            {
+                Console.WriteLine("Mehsul Yoxdur");
+            }
+            else
+            {
+                Console.WriteLine("Mehsul Satisdadir...");
+            }
+
+        }
+
+
+        public void ShowFullData()
+        {
+          
+           
+            Console.WriteLine($"NoteBook adi: {Name}, Descripton:{Description}, Count:{Count}, Stokda Varmi: {IsStock}, Qiymeti:{Price}, " +
+            $"Ram:{RAM}, Hansi Magazadadir:{Storage} ");
+            
+        }
+
+
+
+        public void GetFullData()
+        {
+            Console.WriteLine();
+        }
+
+    }
+
+}
+
+
